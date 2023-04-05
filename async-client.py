1aa721e3f5bafeab6f23f8ae1c284279b45ab47a
Evangelos Stavropoulos <evang.stavropoulos@gmail.com>
2023-04-05 16:52:38 +0200

    Make the client look nicer with rich

--- async-client.py
+++ async-client.py
@@ -1,6 +1,9 @@
 import asyncio
+from datetime import datetime
 from rich.console import Console
+from rich.panel import Panel
 from rich.prompt import Prompt
+from rich.text import Text
 from rich.theme import Theme
 from contextlib import asynccontextmanager
 
@@ -23,8 +26,19 @@ async def receive_message(reader):
     response = await reader.read(1024)
     return response.decode('utf-8')
 
+def display_title():
+    title = Text("Python Chat Client", style="bold white on blue", justify="center")
+    console.print(Panel(title))
+
+def display_welcome_message():
+    welcome_message = Text("Welcome to the Python Chat Client!", style="bold")
+    instructions = Text("Enter your messages below (type 'exit' to quit):")
+    console.print(welcome_message)
+    console.print(instructions)
+
 async def main():
-    console.print("[*] Python Chat Client", style="bold")
+    display_title()
+    display_welcome_message()
 
     host = "127.0.0.1"
     port = 9999
@@ -33,13 +47,16 @@ async def main():
         console.print(f"[*] Connected to {host}:{port}", style="success")
 
         while True:
-            message = Prompt.ask("Enter your message (type 'exit' to quit)")
+            message = Prompt.ask("Enter your message")
             if message.lower() == "exit":
                 break
 
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            console.print(f"[{timestamp}] You: {message}", style="bold blue")
+
             await send_message(writer, message)
             response = await receive_message(reader)
-            console.print(f"Server: {response}", style="bold")
+            console.print(f"[{timestamp}] Server: {response}", style="bold")
 
         console.print("[*] Closing the connection", style="success")
 
