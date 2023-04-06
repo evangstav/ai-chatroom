306babfaef520be430c6404553c6276d37ecf4cf
Evangelos Stavropoulos <evang.stavropoulos@gmail.com>
2023-04-06 12:33:22 +0200

    Only call chatgpt when @bot appears

--- async-server.py
+++ async-server.py
@@ -1,11 +1,12 @@
 import asyncio
 import clients
+from contextlib import asynccontextmanager
 
 
 # client = clients.ClaudeClient()
-client = clients.ChatGPTClient()
+chatgpt = clients.ChatGPTClient()
 
-async def generate_auto_response(message):
+async def generate_auto_response(message, client = None):
     if client:
         return str(client.complete(message)) + "\n\n"
     else:
@@ -18,40 +19,56 @@ async def generate_auto_response(message):
             return "I'm not sure how to respond to that."
 
 
-async def handle_client(reader, writer):
+async def handle_client(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
     """Handles a single client connection and processes its messages."""
     client_address = writer.get_extra_info('peername')
     print(f"[*] New connection from {client_address}")
 
-    while True:
-        try:
-            message = await reader.read(1024)
+    try:
+        while True:
+            message = await read_message(reader)
             if not message:
                 break
-            message = message.decode('utf-8')
             print(f"{client_address}: {message}")
+            if "@chagpt" in message: # Calls bot (chatgpt) if message contains @bot
+                message = message.replace("@bot", "")
+                auto_response = await generate_auto_response(message, chatgpt)
+                await send_message(writer, auto_response)
+            else:
+                auto_response = await generate_auto_response(message)
+                await send_message(writer, auto_response)
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        await close_writer(writer)
+        print(f"[*] Connection closed with {client_address}")
 
-            auto_response = await generate_auto_response(message)
-            print(f"Bot response: {auto_response}")
-            writer.write(auto_response.encode('utf-8'))
-            await writer.drain()
-        except Exception as e:
-            print(f"Error: {e}")
-            break
+async def read_message(reader: asyncio.StreamReader) -> str:
+    message = await reader.read(1024)
+    return message.decode('utf-8')
 
+async def send_message(writer: asyncio.StreamWriter, message: str) -> None:
+    writer.write(message.encode('utf-8'))
+    await writer.drain()
+
+async def close_writer(writer: asyncio.StreamWriter) -> None:
     writer.close()
     await writer.wait_closed()
-    print(f"[*] Connection closed with {client_address}")
 
-async def main():
+async def main(host: str, port: int) -> None:
     """Runs the main server loop, listening for new connections."""
-    server = await asyncio.start_server(handle_client, '0.0.0.0', 9999)
-
-    addr = server.sockets[0].getsockname()
-    print(f"[*] Listening on {addr}")
-
-    async with server:
+    async with create_server(host, port) as server:
+        print(f"[*] Listening on {server.sockets[0].getsockname()}")
         await server.serve_forever()
 
+@asynccontextmanager
+async def create_server(host: str, port: int) -> asyncio.AbstractServer:
+    server = await asyncio.start_server(handle_client, host, port)
+    try:
+        yield server
+    finally:
+        server.close()
+        await server.wait_closed()
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(main('0.0.0.0', 9999))
