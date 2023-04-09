f6861f378a90a43be5c74e9091f6d65cffefe44a
Evangelos Stavropoulos <evang.stavropoulos@gmail.com>
2023-04-09 13:50:44 +0200

    Add some logging and better error handling

--- async-server.py
+++ async-server.py
@@ -1,6 +1,10 @@
 import asyncio
 import clients
 from contextlib import asynccontextmanager
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 bot_dict = {"@claude": clients.ClaudeClient(), "@chatgpt": clients.ChatGPTClient()}
@@ -24,26 +28,28 @@ async def handle_client(
 ) -> None:
     """Handles a single client connection and processes its messages."""
     client_address = writer.get_extra_info("peername")
-    print(f"[*] New connection from {client_address}")
+    logger.info(f"[*] New connection from {client_address}")
 
     try:
         while True:
             message = await read_message(reader)
             if not message:
                 break
-            print(f"{client_address}: {message}")
+            logger.info(f"{client_address}: {message}")
             bot = next((bot for bot in bot_dict if bot in message), None)
-            print(f"bot used: {bot}")
+            logger.info(f"bot used: {bot}")
             if bot:
                 message = message.replace(bot, "")
             auto_response = await generate_auto_response(message, bot_dict.get(bot))
-            print(f"auto response: {auto_response}")
+            logger.info(f"auto response: {auto_response}")
             await send_message(writer, auto_response)
+    except asyncio.IncompleteReadError:
+        logger.warning(f"Disconnected before receiving complete message from {client_address}")
     except Exception as e:
-        print(f"Error: {e}")
+        logger.error(f"Error: {e}")
     finally:
         await close_writer(writer)
-        print(f"[*] Connection closed with {client_address}")
+        logger.info(f"[*] Connection closed with {client_address}")
 
 
 async def read_message(reader: asyncio.StreamReader) -> str:
@@ -68,7 +74,7 @@ async def close_writer(writer: asyncio.StreamWriter) -> None:
 async def main(host: str, port: int) -> None:
     """Runs the main server loop, listening for new connections."""
     async with create_server(host, port) as server:
-        print(f"[*] Listening on {server.sockets[0].getsockname()}")
+        logger.info(f"[*] Listening on {server.sockets[0].getsockname()}")
         await server.serve_forever()
 
 
